022f9f2af893c72dfa66199e1892ecd997da3fc6
dipakpatil5050 <dipakup.a04@gmail.com>
2025-12-12 15:01:36 +0530

    Feat: User Managment Grid layout design setup working

--- IrisAuth/Models/UserModel.cs
+++ IrisAuth/Models/UserModel.cs
@@ -14,5 +14,8 @@ namespace IrisAuth.Models
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Roles { get; set; }
+        public string AvatarUri { get; set; }
     }
 }
